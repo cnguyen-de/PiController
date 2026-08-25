645f2da63b699cd6367a126956d619c080c7b205
Chi Nguyen <c.nguyen.prog@gmail.com>
2026-08-25 23:15:46 +0200

    bla

--- rest-server.py
+++ rest-server.py
@@ -1,33 +1,47 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 import subprocess
+import time
+import datetime
 from subprocess import PIPE
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+from gpiozero import CPUTemperature
 
 fan_status = 1
 
-@app.get("/")
-def read_root():
-    return {"Rest API for Raspberry Pi"}
+def turn_fan_off():
+    print("TURNING FAN OFF", datetime.datetime.now().time())
+    process = subprocess.Popen(['uhubctl','-l', '1-1', '-p' , '2', '-a', '0'], stdout=PIPE, bufsize=-1)
 
-@app.get("/fan/status")
-def read_status():
-    global fan_status
-    return {"fan_status": fan_status}
+def turn_fan_on():
+    print("TURNING FAN ON", datetime.datetime.now().time())
+    process = subprocess.Popen(['uhubctl','-l', '1-1', '-p' , '2', '-a', '1'], stdout=PIPE, bufsize=-1)
 
-@app.get("/fan/control/{status}")
-def control_fan(status: int):
+def checkTemp():
     global fan_status
-    fan_status = status
-    print(status)
-    process = subprocess.Popen(['uhubctl','-l', '1-1', '-p' , '2', '-a', str(fan_status)], stdout=PIPE, bufsize=-1)
-    return {"fan_status": fan_status}
+    temp = CPUTemperature().temperature
+    print(temp)
+    if temp > 55:
+        if fan_status == 0:
+            print("Run fan")
+            fan_status = 1
+            turn_fan_on()
+    elif temp < 48:
+        if fan_status == 1:
+            print("Turn off fan")
+            fan_status = 0
+            turn_fan_off()
+
+def is_night_time():
+    now = datetime.datetime.now().time()
+    start_night_time = datetime.time(22, 0)
+    end_night_time = datetime.time(8, 0)
+    return now >= start_night_time or now < end_night_time
+
+def run_chron():
+    if is_night_time():
+      print("NIGHTY NIGHTY")
+      return
+    
+    turn_fan_on()
+    time.sleep(300)
+    turn_fan_off()
+
+run_chron()
